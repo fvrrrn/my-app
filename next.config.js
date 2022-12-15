737b8f5745a2e447a393304b56d6156b3d7b41c1
fvrrrn <boris.chernystrand@gmail.com>
2022-12-15 17:48:51 +0400

    revert to pages folder structure

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   reactStrictMode: true,
   swcMinify: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/" },
-    };
-  },
 };
 
 module.exports = nextConfig;
